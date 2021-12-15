fe9a5f28e5cd7bbf46719098a9f4ec81535dac38
KirillKrutolevich <k.krutolevichwork@mail.ru>
2021-12-15 16:34:33 +0300

    added Firstsimple and PracticeFormTest test

---
+++ src/test/java/guru/qa/FirstSimpleTest.java
@@ -0,0 +1,15 @@
+package guru.qa;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+@DisplayName("это мой первый простой тест")
+public class FirstSimpleTest {
+
+    @Test
+    void firstTest() {
+        System.out.println("First Test");
+    }
+}
---
+++ src/test/java/guru/qa/PracticeFormTest.java
@@ -0,0 +1,15 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class PracticeFormTest {
+    @Test
+    void RegistrationFormTest() {
+        open("https://demoqa.com/automation-practice-form")
+
+
+    }
+}
