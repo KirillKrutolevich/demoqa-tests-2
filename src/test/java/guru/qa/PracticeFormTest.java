dc8318e50a250039614bf33116ab9274b1183715
KirillKrutolevich <k.krutolevichwork@mail.ru>
2021-12-16 12:27:26 +0300

    .gitignore added

--- src/test/java/guru/qa/PracticeFormTest.java
+++ src/test/java/guru/qa/PracticeFormTest.java
@@ -1,14 +1,22 @@
 package guru.qa;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.cssSelector;
 
 public class PracticeFormTest {
     @Test
     void RegistrationFormTest() {
-        open("https://demoqa.com/automation-practice-form")
+        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Kirill");
+        $("#lastName").setValue("Krutolevich");
+        $("#userEmail").setValue("email@mail.ru");
+        $("#").setValue("");
+        $("#userNumber").setValue("9379992");
+        $("#dateOfBirthInput").click();
 
 
     }
